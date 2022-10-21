4d5527ef7e426c2c18bc48946732cdf4b9d91730
nil <xetaxe@gmail.com>
2022-10-21 21:42:11 +0200

    Generated card pairs, working on its show/hide logic

--- src/GameArea/GameArea.tsx
+++ src/GameArea/GameArea.tsx
@@ -1,26 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import './GameArea.scss';
 import { GameStatusContext, IGameStatusContext } from "../App";
 
 const EMOJI_ARRAY: string[] = ["✌","😂","😝","😁","😱","🙌","🍻","🔥","🌈","🌹","😡","🐶","🐬", "👀","🚗","🍎","💝","👌","😍","😉","😓","😳","💪","💩","🎉","🌺","👠","⚾","🏆","👽","💀","🐵","🐮","🐎","💣","👃","🍓","👊","💋","😘","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🐰","🐍","🐫","🚲","🍉"]
 
+const cardStates: string[] = ["hidden", "clicked", "shown"];
 
 type CardProps = {
-  cardId: number
+  cardId: number,
+  cardContent: string,
+  cardsState: string[]
 }
-
-function Card({cardId}: CardProps) {
-  return (
-    <div className="Card">
-      {EMOJI_ARRAY[cardId]}
-    </div>
-  )
-}
-
 
 type PauseMenuProps = {
   test: string
 }
+
+type EndMenuProps = {
+  test: string
+}
+
+
+
+function Card({cardId, cardContent, cardsState}: CardProps) {
+  const cardShowContent = (cardsState[cardId] === cardStates[0] ? "" : {cardContent});
+  return (
+    <div className="Card">
+      {cardShowContent}
+    </div>
+  )
+}
+
 
 function PauseMenu({test}: PauseMenuProps) {
 
@@ -34,10 +44,6 @@ function PauseMenu({test}: PauseMenuProps) {
 }
 
 
-type EndMenuProps = {
-  test: string
-}
-
 function EndMenu({test}: EndMenuProps) {
   
   const useGameContext: IGameStatusContext = useContext(GameStatusContext);
@@ -50,6 +56,8 @@ function EndMenu({test}: EndMenuProps) {
 }
 
 
+////////////////// WHOLE GAME AREA /////////////////////
+
 type GameAreaProps = {
   numCards: number
 }
@@ -57,9 +65,13 @@ type GameAreaProps = {
 export default function GameArea({numCards}: GameAreaProps) {
 
   const cards: React.ReactElement[] = [];
+  const cardsContent: string[] = RandomCardPairs(EMOJI_ARRAY, numCards);
+  const [cardsState, setCardsState] = useState(cardsContent.map(val => "hidden"));
+
+  console.log(cardsContent);
 
     for(let i:number=0; i<numCards; i++){
-      cards.push(<Card cardId={i}/>);
+      cards.push(<Card cardId={i} cardContent={cardsContent[i]} cardsState={cardsState}/>);
     };
 
   const useGameContext: IGameStatusContext = useContext(GameStatusContext);
@@ -78,4 +90,39 @@ export default function GameArea({numCards}: GameAreaProps) {
     <EndMenu test="test"></EndMenu>
     </div>
   );
+}
+
+
+function RandomCardPairs(referenceArray: string[], numCards: number) {
+  const numPairs: number = numCards/2;
+  let randomIndexPairs: number[] = [];
+  let randomCardPairs: string[] = [];
+  let generatedPairs: number = 0;
+
+  while (generatedPairs < numPairs) {
+    let randomIndex: number = Math.floor(Math.random() * referenceArray.length)
+    if (randomIndexPairs.find(val => val === randomIndex) === undefined) {
+      randomIndexPairs.push(randomIndex, randomIndex)
+      generatedPairs++;
+    }
+  }
+
+  randomIndexPairs = shuffleArray(randomIndexPairs);
+  randomCardPairs = randomIndexPairs.map(val => referenceArray[val]);
+
+  return randomCardPairs;
+
+}
+
+function shuffleArray(array: number[]) {
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
 }
